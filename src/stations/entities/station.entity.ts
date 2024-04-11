433cce0cc868c6e21d5d71b5e617ec08ebf16d3b
ZED-Magdy <zedmagdy@gmail.com>
2024-04-11 08:06:16 +0200

    add station model

--- src/stations/entities/station.entity.ts
+++ src/stations/entities/station.entity.ts
@@ -1,1 +1,24 @@
-export class Station {}
+import { Column, CreateDateColumn, DeleteDateColumn, Point, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class Station {
+    @PrimaryGeneratedColumn()
+    id: number;
+    @Column()
+    name_ar: string;
+    @Column()
+    name_en: string;
+    @Column(
+        "geometry",
+        {
+            spatialFeatureType: "Point",
+            srid: 4326,
+        },
+    )
+    location: Point;
+    @CreateDateColumn()
+    createdAt: Date;
+    @UpdateDateColumn()
+    updatedAt: Date;
+    @DeleteDateColumn()
+    deletedAt: Date;
+}
