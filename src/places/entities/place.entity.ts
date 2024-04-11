3a6a112cfcbaec9816f8bc10d72bb308c6d6004d
ZED-Magdy <zedmagdy@gmail.com>
2024-04-11 08:03:54 +0200

    add scooter model

--- src/places/entities/place.entity.ts
+++ src/places/entities/place.entity.ts
@@ -1,5 +1,6 @@
 import { PriceList } from "src/price-lists/entities/price-list.entity";
 import { Ride } from "src/rides/entities/ride.entity";
+import { Scooter } from "src/scooters/entities/scooter.entity";
 import { Zone } from "src/zones/entities/zone.entity";
 import { Column, CreateDateColumn, DeleteDateColumn, JoinColumn, ManyToOne, MultiPoint, OneToMany, Polygon, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
@@ -24,6 +25,8 @@ export class Place {
     price_list: PriceList;
     @OneToMany(() => Zone, zone => zone.place)
     zones: Zone[];
+    @OneToMany(() => Scooter, scooter => scooter.place)
+    scooters: Scooter[];
     @OneToMany(() => Ride, ride => ride.place)
     rides: Ride[];
     @CreateDateColumn()
--- src/scooters/entities/scooter.entity.ts
+++ src/scooters/entities/scooter.entity.ts
@@ -1,9 +1,38 @@
+import { Place } from "src/places/entities/place.entity";
 import { Ride } from "src/rides/entities/ride.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, ManyToOne, Point, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 export class Scooter {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
+    code: string;
+    @Column()
+    status: string;
+    @Column()
+    battery_level: number;
+    @Column()
+    speed_limit: number;
+    @Column()
+    last_time_connected: Date;
+    @Column()
+    current_speed: number;
+    @Column(
+        "geometry",
+        {
+            spatialFeatureType: "Point",
+            srid: 4326,
+        },
+    )
+    current_location: Point;
+    @ManyToOne(() => Place, place => place.scooters)
+    place: Place;
+    @CreateDateColumn()
     createdAt: Date;
+    @UpdateDateColumn()
     updatedAt: Date;
+    @DeleteDateColumn()
     deletedAt: Date;
+    @ManyToOne(() => Ride, ride => ride.scooter)
     rides: Ride[];
 }
