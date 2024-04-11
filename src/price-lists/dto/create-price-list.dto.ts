a5ec6f54b5a32c074a4f94df6a79a08f506ade3b
ZED-Magdy <zedmagdy@gmail.com>
2024-04-11 07:53:32 +0200

    add price lists

--- src/price-lists/dto/create-price-list.dto.ts
+++ src/price-lists/dto/create-price-list.dto.ts
@@ -1,1 +1,12 @@
-export class CreatePriceListDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreatePriceListDto {
+    @ApiProperty()
+    name_ar: string;
+    @ApiProperty()
+    name_en: string;
+    @ApiProperty()
+    starting_cost: number;
+    @ApiProperty()
+    minute_cost: number;
+}
--- src/price-lists/entities/price-list.entity.ts
+++ src/price-lists/entities/price-list.entity.ts
@@ -1,11 +1,23 @@
 import { Place } from "src/places/entities/place.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
 export class PriceList {
+    @PrimaryGeneratedColumn()
     id: number;
+    @Column()
     name_ar: string;
+    @Column()
     name_en: string;
+    @Column()
+    starting_cost: number;
+    @Column()
+    minute_cost: number;
+    @CreateDateColumn()
     createdAt: Date;
+    @UpdateDateColumn()
     updatedAt: Date;
+    @DeleteDateColumn()
     deletedAt: Date;
+    @OneToMany(() => Place, place => place.price_list)
     places: Place[];
 }
