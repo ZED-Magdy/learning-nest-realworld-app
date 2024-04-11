37232b92033186461864ec2944e375e3c756f52f
ZED-Magdy <zedmagdy@gmail.com>
2024-04-11 07:50:20 +0200

    add places dto create/update

--- src/places/dto/create-place.dto.ts
+++ src/places/dto/create-place.dto.ts
@@ -1,1 +1,16 @@
-export class CreatePlaceDto {}
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CreatePlaceDto {
+    @ApiProperty()
+    name_ar: string;
+    @ApiProperty()
+    name_en: string;
+    @ApiProperty()
+    wkt_string: string;
+    @ApiProperty()
+    color: string;
+    @ApiProperty()
+    z_index: number;
+    @ApiProperty()
+    price_list_id: number;
+}
